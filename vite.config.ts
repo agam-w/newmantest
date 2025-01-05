a76a1b554989b181273d4983b34cb2193268da52
agam-w <agam.w11@gmail.com>
2025-01-06 00:26:10 +0700

    add pwa iconset

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Newman",
+        short_name: "Newman",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "./src/assets/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./src/assets/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
