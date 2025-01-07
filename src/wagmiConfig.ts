632694fd93d40a242c964459c85ae1179362821d
agam-w <agam.w11@gmail.com>
2025-01-07 10:07:51 +0700

    setup wagmi projectId

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,9 +1,11 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { mainnet, arbitrum, sepolia, base } from "wagmi/chains";
 
+const projectId = import.meta.env.VITE_PUBLIC_WALLET_CONNECT_ID ?? "";
+
 export const config = getDefaultConfig({
-  appName: "Newman Test",
-  projectId: "YOUR_PROJECT_ID",
+  appName: "NewmanTest",
+  projectId: projectId,
   chains: [mainnet, arbitrum, sepolia, base],
   ssr: true,
 });
